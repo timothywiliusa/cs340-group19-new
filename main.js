40feba4e51fffa5b599c1ffb0e382c79885568d3
Samarendra M Hedaoo <samarendra.hedaoo@oregonstate.edu>
2018-04-12 17:46:18 -0700

    A better index.html with link to the Website Functioanlity list

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 app.use('/people_certs', require('./people_certs.js'));
-app.use('/people_with_certs', require('./people_certs.js')); //duplicate path
 app.use('/people', require('./people.js'));
+app.use('/', express.static('public'));
 
 app.use(function(req,res){
   res.status(404);
--- people.js
+++ people.js
@@ -76,11 +76,14 @@ module.exports = function(){
     /* Adds a person, redirects to the people page after adding */
 
     router.post('/', function(req, res){
+        console.log(req.body.homeworld)
+        console.log(req.body)
         var mysql = req.app.get('mysql');
         var sql = "INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES (?,?,?,?)";
         var inserts = [req.body.fname, req.body.lname, req.body.homeworld, req.body.age];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
+                console.log(JSON.stringify(error))
                 res.write(JSON.stringify(error));
                 res.end();
             }else{
