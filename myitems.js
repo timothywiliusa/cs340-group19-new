a7febbf3b675ec15fafcbf0e0f6f7261d7238998
Matthew Jacobsen <jacomatt@flip1.engr.oregonstate.edu>
2020-05-29 21:14:33 -0700

    List new item works

--- myitems.js
+++ myitems.js
@@ -20,6 +20,24 @@ module.exports = function(){
         mysql.pool.query(query, handleRenderOfMerch)
     });
     
+    router.post('/', function (req, res){
+        var mysql = req.app.get('mysql');
+        console.log(req.body.title)
+        console.log(req.body.type)
+        console.log(req.body.quantity)
+        console.log(req.body.url)
+        console.log(req.body.cost)
 
+        var inserts = [req.body.title, req.body.url, req.body.type, req.body.quantity, req.body.cost]
+        var query = 'INSERT INTO merch (title, photo, merchType, quantity, price) VALUES (?,?,?,?,?)';
+
+        query = mysql.pool.query(query,inserts,function(error, results, fields){
+            if(error){
+                res.end();
+            }else{
+                res.redirect('/myitems');
+            }
+        });
+    });
     return router;
 }();
